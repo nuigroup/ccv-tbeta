1a3b16316c52bfbc524f98f02c355e1302386543
ss <ss@463ed9da-a5aa-4e33-a7e2-2d3b412cff85>
2009-02-09 22:02:31 +0000

    putting ofxCvConstant.h back to its original state in the OF download.
    git-svn-id: http://nuicode.svnrepository.com/svn/tbeta@31 463ed9da-a5aa-4e33-a7e2-2d3b412cff85

--- trunk/tbeta/OSX/addons/ofxOpenCv/src/ofxCvConstants.h
+++ trunk/tbeta/OSX/addons/ofxOpenCv/src/ofxCvConstants.h
@@ -1,26 +1,16 @@
 #ifndef OFX_CV_CONSTANTS_H
 #define OFX_CV_CONSTANTS_H
 
-#include <iostream>
+
 #include "cv.h"
 #include <vector>
 #include "ofMain.h"
-#include "ofxTBetaCvBlob.h"
-//#include "ofxCvTrackedBlob.h"
 
 
 #define  MAX_NUM_CONTOURS_TO_FIND   500  // alther this if you think you will
                                          // be looking for more then this
                                          // although we use a vector as output
                                          // we currently cap the results to this amount
-class ofCvBlobListener {
-  public:
-  
-    virtual void blobDown( ofxTBetaCvBlob b) = 0;
-    virtual void blobMoved( ofxTBetaCvBlob b) = 0;    
-    virtual void blobUp( ofxTBetaCvBlob b) = 0;
-    
-};
 
 
 #endif
